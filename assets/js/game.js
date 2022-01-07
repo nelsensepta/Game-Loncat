36b19542963b956867a502b854cce8f861988374
nelsesepta <nelseseptahenidar@gmail.com>
2022-01-07 17:43:40 +0700

    Upload

---
+++ assets/js/game.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+  document.getElementById("modal").classList.add("is-visible");
+};
+
+document.getElementById("close-btn").addEventListener("click", () => {
+  // document.getElementById("overlay").;
+  document.getElementById("modal").classList.remove("is-visible");
+});
+
+// document.getElementById("overlay").addEventListener("click", function () {
+//   document.getElementById("overlay").classList.remove("is-visible");
+//   document.getElementById("modal").classList.remove("is-visible");
+// });
+
+// Ambil Input Modal
+let username = document.getElementById("log-username").value;
+let password = document.getElementById("log-password").value;
+document
+  .getElementById("submit")
+  .addEventListener("click", tampilkan_nilai_form);
+
+function tampilkan_nilai_form() {
+  var username = document.getElementById("log-username").value;
+  var password = document.getElementById("log-password").value;
+  if (username === "admin123" && password === "password") {
+    document.location.href = "game.html";
+  }
+}
+
+// Auto Modal
